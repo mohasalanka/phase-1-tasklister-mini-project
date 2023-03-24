91880fd2074de79b120f78a3a30685496d166572
Mohamed <mohasalanka2020@gmail.com>
2023-03-24 12:27:54 +0300

    done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form =document.getElementById('create-task-form')
+
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    let input= document.getElementById('new-task-description').value
+
+
+    let task= document.getElementById('tasks')
+
+    let todo= document.createElement('p')
+    todo.textContent = input
+    task.appendChild(todo)
+
+
+    let button = document.createElement('button')
+    button.textContent='x'
+    todo.appendChild(button)
+
+    button.addEventListener('click',()=>{
+      button.parentNode.remove()
+    })
+
+    
+
+    form.reset()
+  })
+  
 });
